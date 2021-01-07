b131896420281f0f587cb0b5164081ebd914928a
mohilkoladiya <75353022+mohilkoladiya@users.noreply.github.com>
2021-01-07 17:02:53 +0530

    iyti

--- src/Component/Redux/Store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootreducer from './Rootreducer'
-import thunk from 'redux-thunk'
-
-
-export const store = createStore(rootreducer, (applyMiddleware(thunk)));
